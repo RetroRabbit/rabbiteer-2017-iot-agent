11943088b34bb54ee0a5419a56cb273a314c19e9
Cillié Malan <cilliemalan@gmail.com>
2017-10-31 22:12:16 +0200

    metricsHandler takes a logger and fixed message handling

--- handlers/metricsHandler.js
+++ handlers/metricsHandler.js
@@ -1,18 +1,20 @@
 
 class MetricsHandler {
-    constructor(options, mqtt, influx) {
+    constructor(options, mqtt, influx, logger) {
         this._influx = influx;
         this._mqtt = mqtt;
+        this._logger = logger;
     }
 
     get pattern() { return /^metric\/([^/]+)\/([^/]+)$/; }
 
     message(topic, payload, packet) {
 
-        const metric = metricmatch[1];
-        const name = metricmatch[2];
+        const metric = topic[1];
+        const name = topic[2];
         const value = parseFloat(packet.payload);
         if (!isNaN(value) && isFinite(value)) {
+            this._logger.debug(`got metric: ${metric}/${name} -> ${value}`);
             this._influx.writePoints([{
                 measurement: metric,
                 tags: { name },
