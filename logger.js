15c9633f395fe4674ba82fac68a9a7168d414382
Cillié Malan <cilliemalan@gmail.com>
2017-11-08 10:59:38 +0200

    using my own logger

--- logger.js
+++ logger.js
@@ -1,13 +1,23 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf, colorize } = format;
+const { LEVEL, MESSAGE } = require('triple-beam');
 const { Console } = transports;
+const TransportStream = require('winston-transport');
 
+class MyLogger extends TransportStream {
+
+    get name() { return "mylogger"; }
+
+    log(info, callback) {
+        console.log(`${info.level}: ${info.message}`)
+        callback();
+    }
+}
 
 module.exports = (loglevel) => createLogger({
     level: loglevel,
     format: combine(printf(info => {
-        return `${info.level}: ${info.message}`;
-    }),
-        colorize({ all: true })),
-    transports: [new Console()]
+        return info.message;
+    }), colorize({ all: true })),
+    transports: [new MyLogger()]
 });
